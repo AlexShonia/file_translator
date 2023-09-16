4b74f3dd170c36f9ccfcb9cb5dff8e53e7251f69
AlexShonia <alex.shonia123@gmail.com>
2023-09-16 14:12:50 +0400

    turn upload to single file, remove speed

--- file_translator/upload_logic.js
+++ file_translator/upload_logic.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
 var drop_area = document.getElementById('drop_area');
 drop_area.addEventListener('drop', function (e) {
     e.preventDefault();
-    var fileList = e.dataTransfer.files; // the files to be uploaded
-    if (fileList.length == 0) {
+    var file = e.dataTransfer.files[0]; // the file to be uploaded
+    if (!file) {
         return false;
     }
 
@@ -28,29 +28,19 @@ drop_area.addEventListener('drop', function (e) {
 
     // show uploading progress
     var lastTime = Date.now();
-    var lastLoad = 0;
     xhr.upload.onprogress = function (event) {
         if (event.lengthComputable) {
             // update progress
             var percent = Math.floor(event.loaded / event.total * 100);
             document.getElementById('upload_progress').textContent = percent + '%';
 
-            // update speed
-            var curTime = Date.now();
-            var curLoad = event.loaded;
-            var speed = ((curLoad - lastLoad) / (curTime - lastTime) / 1024).toFixed(2);
-            document.getElementById('speed').textContent = speed + 'MB/s'
-            lastTime = curTime;
-            lastLoad = curLoad;
         }
     };
 
-    // send files to server
+    // send file to server
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     var fd = new FormData();
-    for (let file of fileList) {
-        fd.append('files', file);
-    }
+    fd.append('file', file);
     lastTime = Date.now();
     xhr.send(fd);
 }, false);});
